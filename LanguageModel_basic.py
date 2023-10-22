7745513af44501a2db53173da55097e9aac9bdca
Vartika2401 <97868917+Vartika2401@users.noreply.github.com>
2023-10-22 17:37:16 +0530

    something is happening

--- LanguageModel_basic.py
+++ LanguageModel_basic.py
@@ -1,21 +1,165 @@
-from transformers import pipeline
+import requests
+from bs4 import BeautifulSoup
+from langchain.document_transformers import Html2TextTransformer
+from langchain.document_loaders import AsyncHtmlLoader
+from langchain.document_transformers import BeautifulSoupTransformer
+import json
 
-# Load the question-answering model
-qa_pipeline = pipeline("question-answering")
+# import HTML2Text
 
-while True:
-    with open("story.txt", "r") as f:
-        context = f.read()
 
-    question = input("Enter your question: ")
-    if question == "exit":
+base = 'https://www.flipkart.com'
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0',
+    'Accept-Language': 'en-US,en;q=0.9'
+}
+
+product = 'parry-s-white-label-sulphur-free-process-sugar/p/itma70eb903699cf'
+
+product_details = {}
+reviews = []
+
+
+def get_reviews_url(soup):
+    test = soup.find_all('a', href=lambda value: value and 'product-reviews' in value)
+    return test[-1]['href']
+
+
+def get_all_pages(num_pages):
+    # Goto the product page and get the HTML content of the page
+    url = f'{base}/{product}'
+    main_page = requests.get(url, headers=header)
+    # bs_transformer = Html2TextTransformer()
+    print("initial page status code: ", main_page.status_code)
+
+    # with open('spar.html', 'w', encoding="utf-8") as f:
+    #     f.write(page.text)
+    soup = BeautifulSoup(main_page.content, 'lxml')
+
+    # Get the product reviews page url and get the HTML content of the page
+    tmp = get_reviews_url(soup)
+    review_pages = []
+    review_pages.append(url)
+    for i in range(0, num_pages):
+        prod_reviews_url = f'{base}{tmp}&page={i + 1}'
+        review_pages.append(prod_reviews_url)
+
+    product_details['highlights'] = []
+    product_details['product_description'] = []
+    for script in soup.find_all('script', attrs={'id': 'jsonLD'}):
+        json_data = json.loads(script.text)
+        # print(json_data)
+        product_details['rating'] = json_data[0].get('aggregateRating', {}).get('ratingValue', 0)
+        product_details['review_count'] = json_data[0].get('aggregateRating', {}).get('reviewCount', 0)
+        product_details['brand'] = json_data[0].get('brand', {}).get('name', 0)
+        product_details['name'] = json_data[0].get('name', 0)
+        product_details['image'] = json_data[0].get('image', 0)
+        product_details['price'] = json_data[0].get('offers', {}).get('price', 0)
         break
 
-    # Get the answer
-    answer = qa_pipeline(question=question, context=context)
+    try:
+        for li in soup.find_all('p'):
+            # Check for any class, if any reject it
+            if li.has_attr('class'):
+                continue
+            product_details['product_description'].append(li.text.strip())
+        product_details['description'] = product_details['product_description'][1]
+        product_details['product_description'] = product_details['product_description'][2:-13]
+    except:
+        pass
 
-    print("Answer:", answer['answer'])
+    try:
+        tmp = soup.find('div', string='Highlights').find_next_sibling()
+        # for each text in the highlights, add it to the list
+        for li in tmp.find_all('li'):
+            product_details['highlights'].append(li.text.strip())
+    except:
+        pass
+    # print(product_details['highlights'])
 
-# cute isn't it?
+    # Add specifications
+    product_details['specifications'] = {}
+    # for li in tmp.find_all('li'):
+    #     product_details['specifications'].append(li.text.strip())
+    try:
+        tmp = soup.find('div', string='Specifications').find_next_sibling().findChild()
+        # print(tmp.prettify())
+        i = 0
+        for cli in tmp:
+            li = cli
+            curname = li.find('div').text.strip()
+            # print(cli.prettify())
+            # locate the table
+            i = 0
+            table = li.find('table')
+            table_dict = {}
+            for tr in table.find_all('tr'):
+                i += 1
+                try:
+                    table_dict[tr.find('td').text.strip()] = tr.find('td').find_next_sibling().text.strip()
+                except:
+                    table_dict[curname] = tr.find('td').text.strip()
+                    pass
+            product_details['specifications'][curname] = table_dict
+    except:
+        pass
+        # break
+    # print(product_details['specifications'])
+    return review_pages
 
-# Path: LanguageModel_basic.py
+
+def get_review_pages(links):
+    for link in links:
+        rev_page = []
+        page = requests.get(link, headers=header)
+        soup = BeautifulSoup(page.content, 'lxml')
+        for li in soup.find_all('div'):
+            if li.has_attr('class') and len(li['class']) == 0:
+                rev_page.append(li.text.strip())
+        rev_page = rev_page[1:-1]
+        reviews.append(rev_page)
+    return
+
+
+def get_reviews(link):
+    rev_page = []
+    page = requests.get(link, headers=header)
+    soup = BeautifulSoup(page.content, 'lxml')
+    for li in soup.find_all('div'):
+        if li.has_attr('class') and len(li['class']) == 0:
+            rev_page.append(li.text.strip())
+    rev_page = rev_page[1:-1]
+    return rev_page
+
+
+def main():
+    links = get_all_pages(4)[1:]
+    get_review_pages(links)
+    # return
+    loader = AsyncHtmlLoader(links, header)
+    docs = loader.load()
+    print('loaded')
+    html2text = Html2TextTransformer()
+    # soup_transformer = BeautifulSoupTransformer()
+
+    docs = html2text.transform_documents(docs)
+    # for i in range(0, len(links)):
+    #     with open(f'./product/reviews/page{i + 1}.txt', 'w', encoding="utf-8") as f:
+    #         for line in reviews[i]:
+    #             f.write(line)
+    #             f.write('\n')
+    # for i in range(0, len(links)):
+    #     with open(f'./product/langchain_rev/page{i + 1}.txt', 'w', encoding="utf-8") as f:
+    #         f.write(docs[i].page_content)
+            # if(i == 0):
+            # print(docs[i].dict())
+    # print(product_details)
+    with open('product_details.json', 'w') as f:
+        json.dump(product_details, f, indent=4, sort_keys=True)
+    print('done')
+    # Close the loader
+    return
+
+
+main()
